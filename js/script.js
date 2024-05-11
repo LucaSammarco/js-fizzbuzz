44b0091b4d59b46f9717509a23dc95929ddb7559
Il tuo nome <la_tua_email@example.com>
2024-05-11 11:59:24 +0200

    Practicing cycles

--- js/script.js
+++ js/script.js
@@ -6,9 +6,15 @@
 // }
 
 
+const ulElement = document.querySelector("ol")
 
-for (let i = 1; i <= 100 ; i++) {
+for (let i = 1; i <= 10 ; i++) {
    
+    const liElement = document.createElement("li")
+    liElement.classList.add("list-class")
+    liElement.append(i)
+
+    ulElement.appendChild(liElement)
 console.log(i);
     
 }
