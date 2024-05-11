7caf94f0719afb3af0ade9406470d785f1810dce
Il tuo nome <la_tua_email@example.com>
2024-05-11 12:35:21 +0200

    exploring cycles and if elements

--- js/script.js
+++ js/script.js
@@ -6,15 +6,34 @@
 // }
 
 
-const ulElement = document.querySelector("ol")
+// const ulElement = document.querySelector("ol")
 
-for (let i = 1; i <= 10 ; i++) {
+// for (let i = 1; i <= 100 ; i++) {
    
-    const liElement = document.createElement("li")
-    liElement.classList.add("list-class")
-    liElement.append(i)
+//     const liElement = document.createElement("li")
+//     liElement.classList.add("list-class")
+//     liElement.append(i)
 
-    ulElement.appendChild(liElement)
-console.log(i);
+//     ulElement.appendChild(liElement)
+// console.log(i);
     
+// }
+
+
+
+// for (let i = 1000; i >= 0; i--) {
+//     console.log(i)
+// }
+
+let rigaConsole = ""
+
+for (let i = 0; i < 8 ; i++) {
+    if ( i % 2 === 0) {
+        rigaConsole += "fizz"
+    } else {
+        //dispari
+        rigaConsole += "buzz"
+    }
 }
+
+console.log(rigaConsole)
